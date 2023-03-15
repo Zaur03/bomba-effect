cc75ca74a4724791fa66e84109cadc30e8830988
https://github.com/Zaur03/bomba-effect.git <zauragacanli@gmail.com>
2023-03-15 23:48:05 +0400

    first commit

--- utils.js
+++ utils.js
@@ -1,3 +1,14 @@
 const explosionSound = new Audio('explosion.mp3')
 const burningSound = new Audio('burning.mp3')
-const showExplosion = () => document.querySelector('.explosion').classList.add('explosion-start')
+const showExplosion = () => document.querySelector('.explosion.mp3')
+const bomba = document.querySelector('.bomb-image')
+const explosion = document.querySelector('.rope-div')
+const ropeDiv = document.querySelector('.rope-div')
+bomba.addEventListener('click', function(){
+    ropeDiv.classList.add('active')
+    setTimeout(() => {
+       showExplosion()
+       burningSound.play()
+       explosionSound.play() 
+    }, 1000);
+})
